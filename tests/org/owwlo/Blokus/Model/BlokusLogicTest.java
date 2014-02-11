a000283c807cc565a38b85ec3d0deebd2d95ec0b
owwlo <owwlo@163.com>
2014-02-11 02:06:07 -0500

    add empty test cases
    Signed-off-by: owwlo <owwlo@163.com>

--- tests/org/owwlo/Blokus/Model/BlokusLogicTest.java
+++ tests/org/owwlo/Blokus/Model/BlokusLogicTest.java
@@ -12,13 +12,103 @@ public class BlokusLogicTest {
 	public void setUp() throws Exception {
 	}
 
-	@After
-	public void tearDown() throws Exception {
+	@Test
+	public void testInitialMove() {
+		fail();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInitialMove_OtherPalyer() {
+		fail();
 	}
 
+	@Test
+	public void testInitialMoveByWrongPlayer() {
+		fail();
+	}
+
+	@Test
+	public void testInitialMoveFromNonEmptyState() {
+		fail();
+	}
+
+	@Test
+	public void testMoveByWrongPlayer() {
+		fail();
+	}
+
+	@Test
+	public void testPlayerCheating() {
+		fail();
+	}
+
+	@Test
+	public void testCorrectMove() {
+		fail();
+	}
+
+	@Test
+	public void testCorrectMove_DiffColorEdgeConnected() {
+		fail();
+	}
+
+	@Test
+	public void testWin() {
+		fail();
+	}
+
+	@Test
+	public void testIllegalMove_NotConnected() {
+		fail();
+	}
+
+	@Test
+	public void testIllegalMove_EdgeConnected() {
+		fail();
+	}
+
+	@Test
+	public void testIllegalMove_OutOfEdge() {
+		fail();
+	}
+
+	@Test
+	public void testIllegalMove_WrongPlayer() {
+		fail();
+	}
+
+	@Test
+	public void testIllegalMove_Overlay() {
+		fail();
+	}
+
+	@Test
+	public void testPieceRotation() {
+		fail();
+	}
+
+	@Test
+	public void testPieceSelectionCorrect() {
+		fail();
+	}
+
+	@Test
+	public void testPieceSelectionIllegal() {
+		fail();
+	}
+
+	@Test
+	public void testLoadPieceFromString() {
+		fail();
+	}
+
+	@Test
+	public void testBoardListener() {
+		fail();
+	}
+
+	@Test
+	public void testApplyMove() {
+		fail();
+	}
 }
